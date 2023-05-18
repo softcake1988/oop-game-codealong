ef18bd2a9a8d9d152225f9b37f713dd010d8f013
Maria Friedemann <friedemann.maria@gmail.com>
2023-05-18 15:35:50 +0200

    movement of player and obstacles

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ class Player {
         this.domElement.style.height = this.height + "vh";
         this.domElement.style.bottom = this.positionY + "vh";
         this.domElement.style.left = this.positionX + "vw";
+        
         //this.domElement.innerText = "This is the player";
 
         const parentElm = document.getElementById("board");
@@ -43,9 +44,63 @@ class Player {
     } 
 }
 
+class Obstacle {
+    constructor(){
+        this.positionX = 50; // property: horizontal movement in %
+        this.positionY = 100; // property: vertical movement in %
+        this.width = 20;  // property for object width in %
+        this.height = 10;  // property for object height in %
+
+        this.domElement;
+
+        this.createDomElement();
+   
+    }
+    createDomElement() {
+
+        this.domElement = document.createElement("div"); // using stored element from constructor
+
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw"; // give element in browser width with .style
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+        
+        //this.domElement.innerText = "This is the player";
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+
+
+
+    moveDown(){
+        this.positionY = this.positionY -= 1; // -5 is faster than -1
+        this.domElement.style.bottom = this.positionY + "vh"; // apply movement changes also in css
+    }
+
+}
+
 // new instance = calls class Player function
 const player = new Player();
 
+// to be able to be accessed in this scope(setInterval)
+const obstaclesArray = []; // store instances of the class Obstacle = array
+
+// creating new obstacle instances every few seconds stored in an array because it will be many obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArray.push(newObstacle);
+}, 4000)
+
+
+
+// intervall in dem sich jedes Objekt nach unten bewegt, alle 0,6 Sekunden
+setInterval(() => {
+    obstaclesArray.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown(); // using method moveDown 
+    }); 
+}, 60)
 
 
 // add eventListeners to player
